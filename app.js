f0f170351ae292ef315321338e75a28a3110094b
Iloveumom <jtndrbhatia93@gmail.com>
2026-01-09 15:22:42 +0530

    Question-Routes

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+// Global middleware – runs for ALL requests
+app.use((req, res, next) => {
+  console.log(`Request Method: ${req.method}, URL: ${req.url}`);
+  next(); // must call next
+});
+
+// GET /orders
+app.get("/orders", (req, res) => {
+  res.send("Here is the list of all orders.");
+});
+
+// POST /orders
+app.post("/orders", (req, res) => {
+  res.send("A new order has been created.");
+});
+
+// GET /users
+app.get("/users", (req, res) => {
+  res.send("Here is the list of all users.");
+});
+
+// POST /users
+app.post("/users", (req, res) => {
+  res.send("A new user has been added.");
+});
+
+// Start server
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
